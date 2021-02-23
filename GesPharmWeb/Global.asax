566917cbaf6e1cb5abd302de13c4fcd6cae9d0ce
cindyderin27 <61082408+cindyderin27@users.noreply.github.com>
2021-02-23 12:57:44 +0100

    Ajoutez des fichiers projet.

---
+++ GesPharmWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GesPharmWeb.MvcApplication" Language="C#" %>
---
+++ GesPharmWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GesPharmWeb.Startup))]
+namespace GesPharmWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
